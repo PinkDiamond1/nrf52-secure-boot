756ccfcfbd2dcf4126c211b6d6ef5cc9102f8b57
chirag-parmar <chirag.parmar@slock.it>
2019-08-09 16:39:34 +0200

    shift to open_bootloader temporarily

--- include/secure.h
+++ include/secure.h
@@ -1,6 +1,14 @@
 #ifndef __SECURE_H__
 #define __SECURE_H__
 
-void protect_private_key();
+#ifndef DEVICE_SECRET_ADDRESS
+#define DEVICE_SECRET_ADDRESS 0x000E0000
+#endif
+
+#ifndef DEVICE_SECRET_SIZE
+#define DEVICE_SECRET_SIZE 0x00001000
+#endif
+
+void copy_kdr();
 
 #endif
--- src/secure.c
+++ src/secure.c
@@ -1,5 +1,6 @@
 /* NRF52840 Hardware Interface Library. */
 #include "nrf52840.h"
+#include "secure.h"
 
 /*
 * Set the read back protection using Control Access Ports. By specifying it as
@@ -8,15 +9,6 @@
 */
 #define AP_PROTECT 0xFFFFFF00
 const unsigned int *approtect_set __attribute__((section("ctrlap"))) = (unsigned int *) AP_PROTECT;
-
-#define PROTECTED_REGION_ADDRESS 0x000FE000
-#define PROTECTED_REGION_SIZE 0x00001000
-#define PROTECTED_REGION_PERM 0x00000006
-
-#define KDR0_ADDRESS 0x000FE000
-#define KDR1_ADDRESS 0x000FE004
-#define KDR2_ADDRESS 0x000FE008
-#define KDR3_ADDRESS 0x000FE00C
 
 #define PRIVATE_KEY_0 0xDEADBEEF
 #define PRIVATE_KEY_1 0xDEADBEEF
@@ -30,18 +22,13 @@ const unsigned int *my_private_key[4] __attribute__((section(".private_key"))) =
   (unsigned int *) PRIVATE_KEY_3
 };
 
-void protect_private_key() {
+void copy_kdr() {
   //set life cycle state to secure so you can write into KDR registers only once.
   NRF_CC_HOST_RGF->HOST_IOT_LCS = 2;
 
   //copy key from flash to KDR registers
-  NRF_CC_HOST_RGF->HOST_IOT_KDR0 = *((unsigned int *)KDR0_ADDRESS);
-  NRF_CC_HOST_RGF->HOST_IOT_KDR1 = *((unsigned int *)KDR1_ADDRESS);
-  NRF_CC_HOST_RGF->HOST_IOT_KDR2 = *((unsigned int *)KDR2_ADDRESS);
-  NRF_CC_HOST_RGF->HOST_IOT_KDR3 = *((unsigned int *)KDR3_ADDRESS);
-
-  //Enable ACL
-  NRF_ACL->ACL[7].ADDR = PROTECTED_REGION_ADDRESS;
-  NRF_ACL->ACL[7].PERM = PROTECTED_REGION_PERM;
-  NRF_ACL->ACL[7].SIZE = PROTECTED_REGION_SIZE;
+  NRF_CC_HOST_RGF->HOST_IOT_KDR0 = *((unsigned int *)(DEVICE_SECRET_ADDRESS));
+  NRF_CC_HOST_RGF->HOST_IOT_KDR1 = *((unsigned int *)(DEVICE_SECRET_ADDRESS + 0x00000004));
+  NRF_CC_HOST_RGF->HOST_IOT_KDR2 = *((unsigned int *)(DEVICE_SECRET_ADDRESS + 0x00000008));
+  NRF_CC_HOST_RGF->HOST_IOT_KDR3 = *((unsigned int *)(DEVICE_SECRET_ADDRESS + 0x0000000C));
 }
