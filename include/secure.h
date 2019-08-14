da261e38d7357b1a86dc5670b432269350bc1cda
chirag-parmar <chirag.parmar@slock.it>
2019-08-14 14:49:54 +0200

    share variables among linker script and secure.h

--- include/secure.h
+++ include/secure.h
@@ -1,12 +1,15 @@
 #ifndef __SECURE_H__
 #define __SECURE_H__
 
+extern unsigned int _device_secrets_address;
+extern unsigned int _device_secrets_length;
+
 #ifndef DEVICE_SECRET_ADDRESS
-#define DEVICE_SECRET_ADDRESS 0x000FD000
+#define DEVICE_SECRET_ADDRESS (uint32_t) &_device_secrets_address
 #endif
 
 #ifndef DEVICE_SECRET_SIZE
-#define DEVICE_SECRET_SIZE 0x00001000
+#define DEVICE_SECRET_SIZE (uint32_t) &_device_secrets_length
 #endif
 
 void copy_kdr();
